d81b93d7717f484d92e2603cd30bfbc61128c5b2
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 11:55:48 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,11 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="3">
-        <h3 class="text-center">Sign in with Microsoft</h3>
-        <hr>
-        <v-row justify="space-around">
+        <v-container>
+          <h3 class="text-center">Sign in with Microsoft</h3>
+          <hr>
+        </v-container>
+        <v-row justify="space-around" class="py-6">
           <v-btn>Sign in</v-btn>
         </v-row>
       </v-col>
