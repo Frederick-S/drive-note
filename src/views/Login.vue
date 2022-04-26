812feb844c6c6b586bde2349edaf8d103e0eb89c
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 11:41:34 +0800

    sign in ui

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="3">
+        <h3 class="text-center">Sign in with Microsoft</h3>
+        <hr>
+        <v-row justify="space-around">
+          <v-btn>Sign in</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
