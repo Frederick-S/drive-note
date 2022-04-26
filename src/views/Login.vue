5cb5746c76857a021a5cc520bc4f59719c93c000
Frederick-S <mao_xiaodan@hotmail.com>
2022-04-26 11:59:03 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,9 +7,35 @@
           <hr>
         </v-container>
         <v-row justify="space-around" class="py-6">
-          <v-btn>Sign in</v-btn>
+          <v-btn @click="login">Sign in</v-btn>
         </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import * as msal from '@azure/msal-browser'
+
+const msalConfig = {
+  auth: {
+    clientId: '8d47b31f-72c8-4f3d-a34d-a5862eded42c'
+  }
+}
+
+export default {
+  methods: {
+    login () {
+      const msalInstance = new msal.PublicClientApplication(msalConfig)
+
+      msalInstance.loginPopup({
+        scopes: ['user.read']
+      }).then(loginResponse => {
+        console.log(loginResponse)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
