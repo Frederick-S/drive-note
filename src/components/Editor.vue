246b1f35c9afb4da804525b9e2fa8c31ab2a36e3
Frederick-S <mao_xiaodan@hotmail.com>
2022-06-12 16:34:49 +0800

    editor: display only for markdown file

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,13 +1,22 @@
 <template>
   <v-container>
-    <v-md-editor v-model="content" height="500px"></v-md-editor>
+    <v-md-editor v-if="isMarkdownFile" v-model="content" height="500px"></v-md-editor>
   </v-container>
 </template>
 
 <script>
+import { isMarkdownFile } from '../util'
+
 export default {
   name: 'Editor',
   computed: {
+    isMarkdownFile: {
+      get () {
+        const item = this.$store.state.selectedDriveItem
+
+        return item && isMarkdownFile(item.name)
+      }
+    },
     content: {
       get () {
         const item = this.$store.state.selectedDriveItem
